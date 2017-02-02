228d4017d6a08670583299f6636f09e6f503fb06
martov1 <joacobrandan@gmail.com>
2017-02-02 14:48:14 -0300

    chartjs-color-bars.ts

---
+++ chartjs-color-bars.ts
@@ -0,0 +1,17 @@
+    // plugin to change color of bars based on value, is used all over the dashboards
+
+    let barColors = Chart.pluginService.register({
+      beforeUpdate: function(chartInstance) {
+
+
+        chartInstance.data.datasets.forEach(function(dataset) {
+          if (!dataset.colors) { return };
+          let colors = dataset.colors
+          dataset.backgroundColor = dataset.data.map(function(data, key) {
+            return colors[key];
+          })
+        })
+      }
+    });
+
+    export {barColors}
